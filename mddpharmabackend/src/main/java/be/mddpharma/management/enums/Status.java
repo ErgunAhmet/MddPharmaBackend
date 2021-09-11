153355507b9250c4ea9e6fd81527829a8a4459fe
ErgunAhmet <80220613+ErgunAhmet@users.noreply.github.com>
2021-09-11 20:45:44 +0200

    Delete mddpharmabackend directory

--- mddpharmabackend/src/main/java/be/mddpharma/management/enums/Status.java
+++
@@ -1,7 +0,0 @@
-package be.mddpharma.management.enums;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-public enum Status {
-    PRESENT, ALMOST_EMPTY, EMPTY, URGENT
-}
--- mddpharmabackend/src/main/java/be/mddpharma/management/enums/Type.java
+++
@@ -1,7 +0,0 @@
-package be.mddpharma.management.enums;
-
-
-public enum Type {
-    DEBLISTER(),
-    PREPERATION
-}
